1c9f1e551483a0ea24fe70127d4730ceb871c47a
fentosv <fentos.dev@gmail.com>
2024-07-28 16:54:31 +0200

    Update CARDS object copies

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const CARDS: Card[] = [
   { title: 'Title 3', body: 'This is yet another body', variant: 'flamingo-3' },
   { title: 'Title 4', body: 'This is a different body', variant: 'flamingo-4' },
   { title: 'Title 5', body: 'This is a body again', variant: 'flamingo-1' },
-  { title: 'Title 6', body: 'This is another body again', variant: 'flamingo-2' },
-  { title: 'Title 7', body: 'This is yet another body again', variant: 'flamingo-3' },
-  { title: 'Title 8', body: 'This is a different body again', variant: 'flamingo-4' },
+  { title: 'Title 6', body: 'This is different body', variant: 'flamingo-2' },
+  { title: 'Title 7', body: 'This is another body', variant: 'flamingo-3' },
+  { title: 'Title 8', body: 'This is a different body', variant: 'flamingo-4' },
 ]
